64ebd9be7e93262f9fbcfd3d37649a4eb781a1c4
Swalehjamshaid <roy.jamshaid@gmail.com>
2025-12-15 16:50:44 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,16 @@
-
 import os
 
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY", "supersecretkey")
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL", "sqlite:///audit.db")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # >>> ADDED: SQLAlchemy engine options to prevent connection timeouts <<<
+    SQLALCHEMY_ENGINE_OPTIONS = {
+        'pool_pre_ping': True,
+        'pool_recycle': 600
+    }
+    # <<< END ADDED SECTION >>>
 
     MAIL_SERVER = os.environ.get("MAIL_SERVER", "smtp.gmail.com")
     MAIL_PORT = int(os.environ.get("MAIL_PORT", 587))
@@ -15,3 +21,6 @@ class Config:
 
     REDIS_URL = os.environ.get("REDIS_URL", "redis://localhost:6379/0")
     RQ_QUEUE = os.environ.get("RQ_QUEUE", "default")
+Use code with caution.
+
+After you update this file in your project and restart your application container on Railway (or wherever it's deployed), SQLAlchemy will automatic
